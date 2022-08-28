66d583be16187d205ab6d9576c2108ccf6a29b6e
kypanz <kypanz@gmail.com>
2022-08-28 14:06:07 -0300

    adding some changes in the flow for fix getting the image from discord

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import changeImage from './saveWallpaper.js';
 
+// This variable save the last message in the channel
+let lastMessage = null;
+
 // Create a new client instance
 const client = new Client({ 
     intents: [
@@ -24,24 +27,36 @@ client.once('ready', () => {
 // ACTIONS
 client.on('messageCreate', async (msg) => {
     try {
+
         if(msg.channelId != process.env.DETECT_THIS_CHANNEL) return;
-        
-        // if the message dont has image
-        await msg;
-        if(typeof msg.embeds[0].image == 'undefined') return console.log('last message dont has image.');
-        if(typeof msg.embeds[0].image.url == 'undefined') return console.log('last message dont has image.');
-        
-        // if he menssage has image
-        console.log('actual donwloaded image => ',msg.embeds[0].image.url);
-        await changeImage(msg.embeds[0].image.url);
+        console.log('new message id => ',msg.id);
+        lastMessage = msg;
         
     } catch (error) {
         console.log('something wrong ...');
-        console.log(error.reason);
+        if(error.message == "Cannot read properties of undefined (reading 'image')") {
+            console.log('problema con la imagen');
+        }
         console.log('-------');
         console.log('the script still waiting for the next image ...');
     }
+
 });
+
+// This function is used for change the image
+const runAgain = async () => {
+    try {
+        if(lastMessage == null) return console.log('sin imagen a cambiar ...');
+        changeImage(lastMessage.embeds[0].image.url);
+    } catch (error) {
+        console.log('error in runAgain function ...');
+    }
+}
+
+// This interval is for run the "runAgain" function
+setInterval(async () => {
+    await runAgain();
+}, 1000 * 60 * process.env.MINUTES);
 
 // Login to Discord with your client's token
 client.login(process.env.MY_DISCORD_BOT_TOKEN);
--- saveWallpaper.js
+++ saveWallpaper.js
@@ -3,24 +3,26 @@
     and is used for your windows background
 */
 
-import { getWallpaper, setWallpaper } from 'wallpaper';
+import { setWallpaper } from 'wallpaper';
 import fs from 'fs';
 import axios from 'axios';
 
  const changeImage = async (_image) => { 
-    const response = await axios({
-        method:'get',
-        url : _image,
-        responseType : 'arraybuffer'
-    });
-    fs.writeFile('./images/actual_image.jpg', response.data, (err) => {
-        if(err) return console.log('Error al intentar guardar imagen. ',err);
-        console.log('imagen guardada.');
-    });
-    await setWallpaper('./images/actual_image.jpg');
+    try {
+        const response = await axios({
+            method:'get',
+            url : _image,
+            responseType : 'arraybuffer'
+        });
+        fs.writeFile('./images/actual_image.jpg', response.data, (err) => {
+            if(err) return console.log('Error al intentar guardar imagen. ',err);
+            console.log('imagen guardada.');
+        });
+        await setWallpaper('./images/actual_image.jpg');
+    } catch (error) {
+        console.log('error in changeImage function ...');
+        console.log(error);
+    }
 }
-
-//const image = 'https://static01.nyt.com/images/2022/05/10/science/28DOGS-BEHAVIOR1/28DOGS-BEHAVIOR1-mobileMasterAt3x.jpg';
-//changeImage(image);
 
 export default changeImage;
